8de56ff9cccb7f831d17275bf050d04e64d5dad3
James Gregory <James.Gregory@spirit-digital.co.uk>
2022-02-23 12:35:16 +0000

    Fix failing pipeline

--- CTV.Application.SpecFlowAppiumTests/Pages/Consent.cs
+++ CTV.Application.SpecFlowAppiumTests/Pages/Consent.cs
@@ -17,7 +17,6 @@ namespace SpecFlowAppiumTests.Pages
 
         AppiumElement companyLogo => _driver.FindElement(MobileBy.AccessibilityId("companyLogoImage"));
         AppiumElement consentTitle => _driver.FindElement(MobileBy.AccessibilityId("consentTitle"));
-        AppiumElement consentContent => _driver.FindElement(MobileBy.AccessibilityId("consentContent"));
         AppiumElement acceptInput => _driver.FindElement(MobileBy.AccessibilityId("acceptInput"));
         AppiumElement rejectInput => _driver.FindElement(MobileBy.AccessibilityId("rejectInput"));
 
@@ -25,7 +24,7 @@ namespace SpecFlowAppiumTests.Pages
 
         public bool ValidateElements(string elementName)
         {
-            AppiumElement[] appiumWebElements = { companyLogo, consentTitle, consentContent, acceptInput, rejectInput };
+            AppiumElement[] appiumWebElements = { companyLogo, consentTitle, acceptInput, rejectInput };
 
             string locator = Globals.GetLocator();
 
@@ -52,7 +51,6 @@ namespace SpecFlowAppiumTests.Pages
         {
             ScrollToBottom();
             Thread.Sleep(500);
-
             acceptInput.Click();
         }
 
@@ -60,7 +58,6 @@ namespace SpecFlowAppiumTests.Pages
         {
             ScrollToBottom();
             Thread.Sleep(500);
-
             rejectInput.Click();
         }
 
