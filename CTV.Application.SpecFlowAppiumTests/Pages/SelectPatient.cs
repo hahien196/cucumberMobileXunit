5cadfd0471f1375b8f5e012f6910d5a899ea8656
alexspirit <hien.t@spirit-health.com>
2022-04-04 13:14:13 +0700

    Add Select patient page object

---
+++ CTV.Application.SpecFlowAppiumTests/Pages/SelectPatient.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium.Appium;
+using System;
+using SpecFlowAppiumTests.Helpers;
+using OpenQA.Selenium;
+
+namespace SpecFlowAppiumTests.Pages
+{
+    public class SelectPatient
+    {
+        private static AppiumDriver _driver;
+
+        public SelectPatient(AppiumDriver appiumDriver)
+        {
+            _driver = appiumDriver;
+        }
+        
+    }
+}
